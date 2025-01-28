b44ffaf1cc2ddc0db333a28acc37077008fc9508
Lloyd <jalloyd@deloitte.com>
2025-01-28 15:31:24 -0500

    updated deploy method

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -4,44 +4,42 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'SEMOSS',
-  tagline: 'SEMOSS Documentation',
-  favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-
-  onBrokenLinks: 'ignore',
-  onBrokenMarkdownLinks: 'ignore',
+  title: "SEMOSS",
+  tagline: "SEMOSS Documentation",
+  favicon: "img/favicon.ico",
+  organizationName: "SEMOSS",
+  projectName: "documentation",
+  deploymentBranch: "prod",
+  url: "https://semoss.github.io", // Your website URL
+  baseUrl: "/documentation",
+  trailingSlash: false,
+  onBrokenLinks: "ignore",
+  onBrokenMarkdownLinks: "ignore",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
-          routeBasePath: '/',
+          sidebarPath: "./sidebars.js",
+          routeBasePath: "/",
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -51,40 +49,40 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'SEMOSS',
+        title: "SEMOSS",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            position: 'left',
-            label: 'Documentation',
+            position: "left",
+            label: "Documentation",
             items: [
               {
                 label: "Introduction",
-                to: "/"
+                to: "/",
               },
               {
                 label: "Getting Started",
-                to: "category/get-started"
+                to: "category/get-started",
               },
               {
                 label: "How To",
-                to: "category/how-to"
+                to: "category/how-to",
               },
               {
                 label: "Advanced Installation",
-                to: "category/advanced-installation"
-              }
-            ]
+                to: "category/advanced-installation",
+              },
+            ],
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/facebook/docusaurus",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
@@ -94,7 +92,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
-      }
+      },
     }),
 };
 
